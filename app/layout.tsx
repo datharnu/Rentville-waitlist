1e7c205e5257ea772e6c0d323342a2ac09a37c5a
datharnu <olagbemisoyee@gmail.com>
2024-07-15 17:15:59 +0800

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${zilla.variable} ${bitter.variable}`}>
+      <head>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <link
+          rel="shortcut icon"
+          href={`/favicon.ico?v=${new Date().getTime()}`}
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
